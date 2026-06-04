7ea112eae7f41723aead55ed8e7deae9bb349902
Rebecca Lai <rlambert.w@gmail.com>
2026-06-04 16:32:27 +0700

    Basic wordle game

--- src/App.js
+++ src/App.js
@@ -1,48 +1,131 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const SECRET_WORD = "guess" // TODO: get random word from file 
+const WORD_LENGTH = 5
+const GUESS_LENGTH = 6
 
 export default function Game() {
-  
-  function handlePlay() {
+  const [announcement, setAnnouncement] = useState('Guess another word!');
+  const [activeRow, setActiveRow] = useState(0); // index of active row
+  const [guessedWords, setGuessedWords] = useState(new Array(GUESS_LENGTH).fill(new Array(WORD_LENGTH).fill(' ')));
+  const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
+  const [currentWord, setCurrentWord] = useState(new Array(WORD_LENGTH).fill(' '));
+  const [gameOver, setGameOver] = useState(false);
+
+      
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentLetterIndex, currentWord, activeRow]);
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      handleEnter();
+    } else if (e.key === 'Backspace') {
+      handleBackspace();
+    } else if ('abcdefghijklmnopqrstuvwxyz'.includes(e.key)) {
+      handleLetter(e.key);
+    }
   }
 
-  let gameOver = false; // set in handlePlay
-  let message = "Guess another word!"
-  if (gameOver) {
-    let didWin = true; // set in handlePlay
-    message = didWin ? "You won!" : "You lost :("
+  function handleEnter() {
+    if (currentLetterIndex < WORD_LENGTH) return;
+
+    if (currentWord.join("") === SECRET_WORD) {
+      setAnnouncement("You won!");
+      setGameOver(true);
+    } else if (activeRow === GUESS_LENGTH - 1) {
+      setAnnouncement("You lost :("); 
+      setGameOver(true);
+    } else {
+      const guessedWordsListCopy = [...guessedWords];
+      guessedWordsListCopy[activeRow] = currentWord;
+      
+      setGuessedWords(guessedWordsListCopy)
+      setCurrentWord(new Array(WORD_LENGTH).fill(" "))
+      setActiveRow(activeRow => activeRow + 1); 
+      setCurrentLetterIndex(0)
+    }
+  }
+
+  function handleLetter(letter) {
+    if (currentLetterIndex >= WORD_LENGTH) return;
+
+    const currentWordCopy = [...currentWord];
+    currentWordCopy[currentLetterIndex] = letter;
+
+    setCurrentWord(currentWordCopy);
+    setCurrentLetterIndex(currentLetterIndex => currentLetterIndex + 1);
+  }
+
+  function handleBackspace() {
+    if (currentLetterIndex === 0) return;
+
+    const currentWordCopy = [...currentWord];
+    currentWordCopy[currentLetterIndex - 1] = ' ';
+
+    setCurrentWord(currentWordCopy);
+    setCurrentLetterIndex(currentLetterIndex => currentLetterIndex - 1);
+  }
+
+  function replayGame() {
+    setAnnouncement('Guess another word!');
+    setActiveRow(0)
+    setGuessedWords(new Array(GUESS_LENGTH).fill(new Array(WORD_LENGTH).fill(' ')));
+    setCurrentLetterIndex(0);
+    setCurrentWord(new Array(WORD_LENGTH).fill(' '));
+    setGameOver(false);
   }
 
   return (
     <div className="game">
-      <text className="announcement">{message}</text>
+      <p className="announcement">{announcement}</p>
       <div className="game-board">
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
+        {guessedWords.map((word, i) => {
+          if (activeRow == i) {
+            return (
+              <Row word={currentWord} isActive={true} />
+            )
+          } else {
+            return (
+              <Row word={word} isActive={false} />
+            )
+          }
+        })}
       </div>
+        {gameOver && <button onClick={replayGame}>Replay?</button>}
     </div>
   )
 }
 
-function Row() {
-
+function Row({word, isActive}) {
   return (
-    <div className='row'>
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+    <div className={`row ${isActive && "active"}`}>
+      {word.map((letter, i) => {
+        return (
+          <Square index={i} letter={letter} isActive={isActive} />
+        )
+      })}
     </div>
   )
 }
 
-function Square() {
+function Square({letter, isActive, index}) {
+  let color;
+  if (!isActive) {
+    if (SECRET_WORD[index] == letter) {
+      color = 'correct'
+    } else if (SECRET_WORD.includes(letter)) {
+      color = 'almost'
+    } else if (letter !== " ") {
+      color = 'wrong'
+    }
+  }
 
   return (
-    <input className='square' type="text" maxlength="1" />
+    <div className={`square ${color}`}>{letter}</div>
   )
 }
